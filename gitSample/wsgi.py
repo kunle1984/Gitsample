8b6ab0be8a20c4acdcf99d072fe4f517ba8fdb2d
kunle <kunlefes089@gmail.com>
2022-10-24 11:40:56 +0100

    First commit

---
+++ gitSample/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gitSample project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitSample.settings')
+
+application = get_wsgi_application()
